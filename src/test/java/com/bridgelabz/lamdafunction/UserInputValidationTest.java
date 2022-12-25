7b00430f0661fce44b9994f4bcd4ace8d81f11f6
komalarao <manoharvakamullu1999@gmail.com>
2022-12-25 09:05:30 -0800

    add: program to email pattern test case

--- src/test/java/com/bridgelabz/lamdafunction/UserInputValidationTest.java
+++ src/test/java/com/bridgelabz/lamdafunction/UserInputValidationTest.java
@@ -1,25 +1,48 @@
 package com.bridgelabz.lamdafunction;
+import org.junit.Assert;
+import org.junit.Test;
 
 
-	import org.junit.Assert;
-	import org.junit.Test;
+public class UserInputValidationTest {
+	@Test
+    public void givenFirstName_WhenProper_ShouldReturnTrue() {
+        UserInputValidation userInputValidation = new UserInputValidation();
+        boolean result = UserInputValidation.isValidFirstName("Dipali");
+        Assert.assertEquals(true, result);
+    }
+    @Test
+    public void givenFirstName_WhenShort_ShouldReturnFalse() {
+        UserInputValidation userInputValidation = new UserInputValidation();
+        boolean result = UserInputValidation.isValidFirstName("Pranjal");
+        Assert.assertEquals(false, result);
+    }
+    @Test
+    public void givenLastName_WhenProper_ShouldReturnTrue() {
+        UserInputValidation userInputValidation = new UserInputValidation();
+        boolean result = UserInputValidation.isValidLastName("bhuwan");
+        Assert.assertEquals(true, result);
+    }
 
-	public class UserInputValidationTest {
-		@Test
-	    public void givenFirstName_WhenProper_ShouldReturnTrue() {
-	        UserInputValidationTest userInputValidationTest = new UserInputValidationTest();
-	        boolean result = userInputValidationTest.isValidFirstName("komal");
-	        Assert.assertEquals(true, result);
-	    }
-	    private boolean isValidFirstName(String string) {
-			// TODO Auto-generated method stub
-			return false;
-		}
-		@Test
-	    public void givenFirstName_WhenShort_ShouldReturnFalse() {
-	        UserInputValidationTest userInputValidationTest = new UserInputValidationTest();
-	        boolean result = userInputValidationTest.isValidFirstName("Ko");
-	        Assert.assertEquals(false, result);
-	    }
-	}
+    @Test
+    public void givenLastName_WhenShort_ShouldReturnFalse() {
+        UserInputValidation userInputValidation = new UserInputValidation();
+        boolean result = UserInputValidation.isValidLastName("So");
+        Assert.assertEquals(false, result);
+    }
+    @Test
+    public void givenEmail_WhenValid_ShouldReturnTrue() {
+        UserInputValidation userInputValidation = new UserInputValidation();
+        boolean result = UserInputValidation.isValidEmail("aryabhuwan873@gmail.com");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenEmail_WhenNotValid_ShouldReturnFalse() {
+        UserInputValidation userInputValidation = new UserInputValidation();
+        boolean result = UserInputValidation.isValidEmail("bhuwanarya...873.gmail.com");
+        Assert.assertEquals(false, result);
+    }
+
+}
+
 
