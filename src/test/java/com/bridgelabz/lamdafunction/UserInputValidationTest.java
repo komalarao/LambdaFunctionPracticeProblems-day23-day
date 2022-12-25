4d5adf93935080423d890da8b6800b2b204fcd82
komalarao <manoharvakamullu1999@gmail.com>
2022-12-25 08:53:46 -0800

    add: program to test case of last name

---
+++ src/test/java/com/bridgelabz/lamdafunction/UserInputValidationTest.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.lamdafunction;
+
+
+	import org.junit.Assert;
+	import org.junit.Test;
+
+	public class UserInputValidationTest {
+		@Test
+	    public void givenFirstName_WhenProper_ShouldReturnTrue() {
+	        UserInputValidationTest userInputValidationTest = new UserInputValidationTest();
+	        boolean result = userInputValidationTest.isValidFirstName("komal");
+	        Assert.assertEquals(true, result);
+	    }
+	    private boolean isValidFirstName(String string) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+		@Test
+	    public void givenFirstName_WhenShort_ShouldReturnFalse() {
+	        UserInputValidationTest userInputValidationTest = new UserInputValidationTest();
+	        boolean result = userInputValidationTest.isValidFirstName("Ko");
+	        Assert.assertEquals(false, result);
+	    }
+	}
+
