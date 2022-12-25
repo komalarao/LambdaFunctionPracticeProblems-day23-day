6aad478de1a63189cff627f5ebe2c93f833cb7b2
komalarao <manoharvakamullu1999@gmail.com>
2022-12-25 08:22:57 -0800

    add:welcome message to userRegistration

---
+++ src/main/java/com/bridgelabz/lamdafunction/App.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.lamdafunction;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "welcome to user registration program" );
+    }
+}
